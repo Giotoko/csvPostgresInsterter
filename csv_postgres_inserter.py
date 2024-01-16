1d33fcc5b6b51dd34366feac0bfb64d64fae4b89
Jhonaiquel Rodríguez <46872129+Giotoko@users.noreply.github.com>
2024-01-16 10:49:53 -0400

    improved perfomance, once again
    times of the previous attempts
    attepmt 1 : 2 hours runtime
    attempt 2 : 28 minutes runtime
    attempt 3 : 9 minutes runtime

--- csv_postgres_inserter.py
+++ csv_postgres_inserter.py
@@ -1,9 +1,8 @@
-from csv import reader, Sniffer, DictReader
-from psycopg2 import connect, sql
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
 from time import time
 import dask.dataframe as dataframe
+from sqlalchemy import DATE, INT, String, create_engine
 
 load_dotenv(find_dotenv())
 
@@ -12,13 +11,12 @@ start_time = time()
 hostname = getenv("hostname")
 username = getenv("username")
 password = getenv("password")
+database = getenv("database")
 
-database = "bita"
 table = "stocks"
 csv_path = "stock.csv"
 
-conn = connect(host=hostname, database=database, user=username, password=password)
-cur = conn.cursor()
+engine = create_engine(f'postgresql://{username}:{password}@{hostname}/{database}')
 
 data = dataframe.read_csv(csv_path)
 
@@ -26,35 +24,14 @@ print("opened in ", time() - start_time, " seconds")
 
 data = data.compute()
 
-batch_size = 1000  
-batch = []
+data.to_sql(table, engine, if_exists='replace', index=False, dtype={
+    'pointOfSale':String(255),
+    'product': String(255),
+    'date':DATE,
+    'stock':INT 
+})
 
-for index, row in data.iterrows():
-    batch.append(row)
-    if i % batch_size == 0:
-        cur.execute(
-            sql.SQL("INSERT INTO {} VALUES {}").format(
-                sql.Identifier(table),
-                sql.SQL(', ').join(
-                    sql.SQL('({})').format(sql.SQL(', ').join(map(sql.Literal, row))) for row in batch
-                )
-            )
-        )
-        conn.commit()
-        batch = []
-
-if batch:
-    cur.execute(
-        sql.SQL("INSERT INTO {} VALUES {}").format(
-            sql.Identifier(table),
-            sql.SQL(', ').join(
-                sql.SQL('({})').format(sql.SQL(', ').join(map(sql.Literal, row))) for row in batch
-            )
-        )
-    )
-    conn.commit()
-cur.close()
-conn.close()
+engine.dispose()
 
 total_time = time() - start_time
 print ("finished in ", total_time, " seconds ")
