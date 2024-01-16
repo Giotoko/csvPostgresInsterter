6fd4c29f373313f1c6710b1e0c783cfc62e7ca7e
Jhonaiquel Rodríguez <46872129+Giotoko@users.noreply.github.com>
2024-01-16 10:13:04 -0400

    greatly improved performance
    still not using any batch operation, perfomance still unusable

--- csv_postgres_inserter.py
+++ csv_postgres_inserter.py
@@ -1,8 +1,9 @@
-from csv import reader, Sniffer
+from csv import reader, Sniffer, DictReader
 from psycopg2 import connect, sql
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
 from time import time
+import dask.dataframe as dataframe
 
 load_dotenv(find_dotenv())
 
@@ -19,27 +20,42 @@ csv_path = "stock.csv"
 conn = connect(host=hostname, database=database, user=username, password=password)
 cur = conn.cursor()
 
-with open(csv_path, 'r') as csv_file:
-    sniffer = Sniffer()
-    delimiter = sniffer.sniff(csv_file.read(1000))
-    read = reader(csv_file, delimiter=delimiter.delimiter)
-    i=0
-    next(read)
-    for row in read:
-        i+=1
-        print("still running ", i)
+data = dataframe.read_csv(csv_path)
+
+print("opened in ", time() - start_time, " seconds")
+
+data = data.compute()
+
+batch_size = 1000  
+batch = []
+
+for index, row in data.iterrows():
+    batch.append(row)
+    if i % batch_size == 0:
         cur.execute(
-            sql.SQL("INSERT INTO {} VALUES ({})").format(
+            sql.SQL("INSERT INTO {} VALUES {}").format(
                 sql.Identifier(table),
-                sql.SQL(', ').join(map(sql.Literal, row))
+                sql.SQL(', ').join(
+                    sql.SQL('({})').format(sql.SQL(', ').join(map(sql.Literal, row))) for row in batch
+                )
             )
         )
-conn.commit()
+        conn.commit()
+        batch = []
+
+if batch:
+    cur.execute(
+        sql.SQL("INSERT INTO {} VALUES {}").format(
+            sql.Identifier(table),
+            sql.SQL(', ').join(
+                sql.SQL('({})').format(sql.SQL(', ').join(map(sql.Literal, row))) for row in batch
+            )
+        )
+    )
+    conn.commit()
 cur.close()
 conn.close()
 
-end_time = time()
-
-total_time = end_time - start_time
+total_time = time() - start_time
 print ("finished in ", total_time, " seconds ")
 
